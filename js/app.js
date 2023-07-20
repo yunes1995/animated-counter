ddace7d9e14f37b8a22ceb97128ef09118a59891
yunes moahammadi <younesm837@gmail.com>
2023-07-20 15:20:23 +0330

    change color when progress is 100

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@ let x = 0;
 const y = 100;
 let starPositon = false;
 
+let getprogressiconOne = document.getElementById("progressIconOne");
+let getprogressiconTwo = document.getElementById("progressIconTwo");
+let getprogressiconThree = document.getElementById("progressIconThree");
+let getprogressiconFour = document.getElementById("progressIconFour");
+let getInprogressText = document.getElementById("inprogressText");
+
 const getNumberOne = document.getElementById("numberOne");
 const getNumberTwo = document.getElementById("numberTwo");
 const getNumberThree = document.getElementById("numberThree");
@@ -25,6 +31,11 @@ function counterNumber() {
     getNumberFour.innerText = x;
 
     if (x == y) {
+        getprogressiconOne.style.color = "green"; 
+        getprogressiconTwo.style.color = "green"; 
+        getprogressiconThree.style.color = "green"; 
+        getprogressiconFour.style.color = "green"; 
+        getInprogressText.style.color = "red"; 
         clearInterval(myInterval);
         return;
     }
